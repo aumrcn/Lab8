4be721b7bda4e811d29d8aa372e594d81af798e8
aumrcn <aumkung20728@gmail.com>
2021-01-05 15:27:58 +0700

    lab8_1 completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	int i = 1;
+	double prevbal, interest, total, newbal, intrate, ppy;
+	cout << "Enter initial loan: ";
+	cin >> 	prevbal;
+	cout << "Enter interest rate per year (%): ";
+	cin >>  intrate;
+	cout << "Enter amount you can pay per year: ";
+	cin >>  ppy;
+	newbal = prevbal;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +20,25 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	while(newbal > 0){
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << prevbal;
+		interest = prevbal*(intrate/100);
+		cout << setw(13) << left << interest;
+		total = prevbal+interest;
+		cout << setw(13) << left << total;
+		newbal = total - ppy;
+		if(newbal < 0){
+			ppy = total;
+			newbal = 0;
+		}
+		cout << setw(13) << left << ppy;
+		cout << setw(13) << left << newbal;
+		prevbal = newbal;
+		cout << "\n";
+		i++;	
+	}
+
 	return 0;
 }
